4734d7bde777ee9eba21b065540176d0367825d1
cadenburleson <cadenburleson@cadens-mbp.gateway.pace.com>
2015-05-26 16:56:16 -0500

    More Restructuring / Learning
    Learned better abstraction and databasing

--- myDatabase.js
+++ myDatabase.js
@@ -1,80 +1,171 @@
+var db;
 
-// var idb = window.indexedDB.open('TimerData', 1);
-var idb = window.indexedDB.open('TimerData', 1);
+// Used to access all your trackers (arrays)
+// var newTracker = [{
+//   url: itemURL,
+//   hours: timeLimitHrs,
+//   minutes: timeLimitMins
+// }];
 
-var newTracker = [{
-    // url: "http://the-site-you-want-to-track.com",
-    // hours: 19,
-    // minutes: 30,
-    url: itemURL,
-    hours: timeLimitHrs,
-    minutes: timeLimitMins,
+function indexedDBOk(){
+  return "indexedDB" in window;
+}
 
-}];
+// Checks if you can use indexedDB
+document.addEventListener("DOMContentLoaded", function() {
+  // No IndexedDB suppport.
+  if (!indexedDBOk) return;
 
-idb.onsuccess = function(evt){
-  // store the result of opening the database in the db variable. This is used a lot below
-  // db = evt.target.result;
-  db = idb.result;
-  // addData();
-  console.log('database successfully created');
-};
+  // Opens a request to open a database
+  var DBOpenRequest = window.indexedDB.open("TimerData", 1);
 
-// This event handles the event whereby a new version of the database needs to be created
-// Either one has not been created before, or a new version number has been submitted via the
-// window.indexedDB.open line above
-// it is only implemented in recent browsers
-idb.onupgradeneeded = function(evt) {
-  var db = evt.target.result;
-  var objectStore = db.createObjectStore("timers", { autoIncrement: true });
-  console.log('onupgradeneeded success');
-  // Use transaction oncomplete to make sure the objectStore creation is 
-  // finished before adding data into it.
-  objectStore.transaction.oncomplete = function(event) {
-    // Store values in the newly created objectStore.
-    // var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
-    // addData();
-    console.log('object store created');
+  // This event handles the event whereby a new version of the database needs to be created
+  // Either one has not been created before, or a new version number has been submitted via the
+  // window.indexedDB.open line above
+  // it is only implemented in recent browsers
+  DBOpenRequest.onupgradeneeded = function(evt) {
+    var thisDB = evt.target.result;
+
+    console.log("Database Upgraded = Success");
+
+    // Only able to define object stores in this function so I've heard?
+    if (!thisDB.objectStoreNames.contains("timers")) {
+      thisDB.createObjectStore("timers", { autoIncrement: true });
+      console.log('Database-ObjectStore / "timers" / Created = Success');
+      // os.creatIndex("url", "url", { unique : true });
+
+    }
+
   }
 
+  // Returns the open database onsucess
+  DBOpenRequest.onsuccess = function(evt) {
+    // store the result of opening the database in the db variable. This is used a lot below
+    // db = DBOpenRequest.result;
+    db = evt.target.result;
+
+    console.log("Database Created = Success");
+    console.dir(evt.target.result);
+    console.log("Current Object Stores : ");
+    console.dir(db.objectStoreNames);
+  };
+
+  DBOpenRequest.onerror = function(evt) {
+    console.log('Database Created = Failed');
+  }
+
+}, false);
+
+
+function deleteTimerDatabase() {
+  var deleteData = indexedDB.deleteDatabase('TimerData');
+  console.log('Timer Database Deleted Successfully');
 };
 
-function addData() {
 
-  // open a read/write db transaction, ready for adding the data
-  // var transaction = db.transaction(['timers'], 'readwrite').objectStore("timers");
-  var transaction = db.transaction(['timers'], 'readwrite');
+var startDate = Math.floor(Date.now() / 1000);
+
+
+function addTimerData() {
+
+  var url = $("#itemURL").val();
+  var hours = $("#timeLimitHrs").val();
+  var minutes = $("#timeLimitMins").val();
+
+  console.log("About to add :" + url + "/" + hours + "/" + minutes);
+
+  //get transaction
+  var transaction = db.transaction(["timers"], "readwrite");
+  //ask for store
+  var store = transaction.objectStore("timers");
+
+  //define a timer
+  var timer = {
+    url: url,
+    hours: hours,
+    minutes: minutes
+  }
+
+  //perform the add
+  // var request = store.put
+  var request = store.add(timer);
+
+  request.onerror = function(e) {
+    console.log("error", e.target.error.name);
+    
+  }
+  request.onsuccess = function(e) {
+    console.log("successfully added a timer");
+  }
+
+}
+
+function getTimerData(x) {
+
+  // x = $("#itemURL").val();
+
+  var transaction = db.transaction(["timers"], "readonly");
+  var store = transaction.objectStore("timers");
+
+  var getRequest = store.get(x);
+
+  getRequest.onsuccess = function(e) {
+    var result = e.target.result;
+    console.dir(result);
+    // console.log("getTimerData :" + result.url);
+  }
+
+}
+
+// function addData() {
+
+//   // loadData();
+//   //Get a transaction
+//   // open a read/write db transaction, ready for adding the data
+//   var transaction = db.transaction(['timers'], 'readwrite');
+//   // report on the success of opening the transaction
+//   transaction.oncomplete = function(event) { console.log('addData() transaction = success'); };
+//   transaction.onerror = function(event) { console.log('addData() transaction = fail'); };
+//   //Ask for the objectStore
+//   var objectStore = transaction.objectStore("timers");
   
-  // report on the success of opening the transaction
-    transaction.oncomplete = function(event) {
-      console.log('trans success');
-    };
-    transaction.onerror = function(event) {
-      console.log('trans fail');
-    };
+//   newTracker[0].startTime = startDate;
+//   newTracker[0].url = itemURL;
+//   newTracker[0].hours = timeLimitHrs;
+//   newTracker[0].minutes = timeLimitMins;
 
-  // create an object store on the transaction  
-  var objectStore = transaction.objectStore('timers');
+//   // add our newItem object to the object store
+//   var objectStoreRequest = objectStore.put(newTracker[0]);
 
-  objectStore.onerror = function(event) {
-    console.log('transaction error');
+//   objectStoreRequest.onsuccess = function(event) {
+//     // report the success of our new item going into the database
+//     console.log('new item added successfully');
+//   }
+
+//   getData();
+
+// }//END OF addData(){}
+
+
+var urls = [];
+
+function getData() {
+
+  var objectStore = db.transaction("timers").objectStore("timers");
+  // var request = objectStore.get(newTracker.url);
+
+  objectStore.openCursor().onsuccess = function(event) {
+    var cursor = event.target.result;
+    if (cursor) {
+      alert("Name for SSN " + cursor.key + " is " + cursor.value.newTracker[0].itemURL);
+      cursor.continue();
+    }
+    else {
+      alert("No more entries!");
+    }
   };
-  objectStore.onsuccess = function(event) {
-    console.log('transaction success');
-  };
 
-  newTracker[0].url = itemURL;
-  newTracker[0].hours = timeLimitHrs;
-  newTracker[0].minutes = timeLimitMins;
 
-  // add our newItem object to the object store
-  var objectStoreRequest = objectStore.put(newTracker[0]);
-  // objectstore.add(newTracker[0]);
+}
 
-  objectStoreRequest.onsuccess = function(event) {
-    // report the success of our new item going into the database
-    console.log('new item added successfully');
-  }
-
-}//END OF addData(){}
 
--- timeTrekrr.js
+++ timeTrekrr.js
@@ -3,8 +3,19 @@ var timeLimitHrs;
 var timeLimitMins;
 
 $('#btnCreateNewTimer').click(function() {
+	// turns off the timer list view
 	$('#mainView').css('display','none');
+	// tuens on the create timer view
 	$('#newTimerView').css('display','block');
+});
+
+$('#btnDeleteTimerData').click(function() {
+	window.deleteTimerDatabase();
+});
+
+
+$('#btnGetTimerData').click(function() {
+	window.getTimerData(1);
 });
 
 $('#app_description_close_button').click(function() {
@@ -12,25 +23,17 @@ $('#app_description_close_button').click(function() {
 });
 
 $('#btnAdd').click( function() {
-// Assigns your input into the html fields to the variables
-	itemURL = $('#itemURL').val();
-	timeLimitHrs = $('#timeLimitHrs').val();
-	timeLimitMins = $('#timeLimitMins').val();
 
-	var outputTimeString = $('#outputTimeString');
+	console.log("btn Add pressed");
+
+	addTimerData();
 
 	$('#timerContainerBitches').append(timerProgressBar);
-
 	$('#mainView').css('display','block');
 	$('#newTimerView').css('display','none');
 
-		console.log('URL: ' + itemURL);
-		console.log('HRS:' + timeLimitHrs);
-		console.log('MINS: ' + timeLimitMins);
-
-		addData();
-
 } );
+
 
 var timerProgressBar = '<div class="progress progress-striped"><div class="progress-bar progress-bar-custom" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"><span class="sr-only">60% Complete</span><H3>Facebook</H3></div></div>';
 
@@ -39,6 +42,9 @@ var timerProgressBar = '<div class="progress progress-striped"><div class="progr
 $('#newTimerCreatorLink').click(function(){
 	$('#timerContainerBitches').append(timerProgressBar);
 });
+
+
+
 
 function getMonth() {
 	var now = new Date(); 
@@ -49,6 +55,10 @@ function getMonth() {
 
 	return month;
 }
+
+// function updateTimerView () {
+// 	function 
+// }
 
 
 
